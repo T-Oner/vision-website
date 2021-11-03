f3d63b34ca4130e59bb2bc242b4da7b0bc00f8e8
Kai Shao <grepug@icloud.com>
2021-11-03 16:29:12 +0800

    update campgin

--- api/app/additional-release-message.js
+++ api/app/additional-release-message.js
@@ -1,12 +1,14 @@
 module.exports = (req, res) => {
   res.json({
     zh: {
-      message: '双十一促销活动',
-      urlString: 'https://okr.vision/zh',
-      buttonText: '查看活动',
+      title: '双十一暨周年庆促销活动',
+      message: '- 永久解锁兑换码 ¥78.00，限量 90 个\n- 一年订阅兑换码 ¥38.00',
+      urlString: 'https://okr.vision/zh/blog/2021/11/03/promo-campaign',
+      buttonText: '活动详情',
       showing: true,
     },
     en: {
+      title: '',
       message: '',
       urlString: '',
       buttonText: '',
