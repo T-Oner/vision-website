0a8386b052671ebe6566f72af7fac9d85ee0eae9
Kai Shao <grepug@icloud.com>
2021-11-04 12:46:00 +0800

    hide campaign for review

--- api/app/additional-release-message.js
+++ api/app/additional-release-message.js
@@ -1,13 +1,12 @@
 module.exports = (req, res) => {
   res.json({
     zh: {
-      title: '双十一暨周年庆促销活动',
-      message:
-        '- 永久解锁兑换码 ¥79.00（原价¥108.00），限量 95 个\n- 一年订阅兑换码 ¥32.00（原价¥68.00）\n- 11 月 6 日 20:00 更可在群内参与抽奖，以更低价格购买',
-      urlString: 'https://okr.vision/zh/blog/2021/11/03/promo-campaign',
-      buttonText: '活动详情',
-      proEntranceSecondaryText: '🔥 促销中',
-      showing: true,
+      title: '',
+      message: '',
+      urlString: '',
+      buttonText: '',
+      proEntranceSecondaryText: '',
+      showing: false,
     },
     en: {
       title: '',
