1dc7619bfe8955c267b45fc389e395ec6be957e9
Kai Shao <grepug@icloud.com>
2021-11-04 12:16:47 +0800

    update compagin message

--- api/app/additional-release-message.js
+++ api/app/additional-release-message.js
@@ -3,7 +3,7 @@ module.exports = (req, res) => {
     zh: {
       title: '双十一暨周年庆促销活动',
       message:
-        '- 永久解锁兑换码 ¥79.00，限量 95 个\n- 一年订阅兑换码 ¥32.00\n- 11 月 6 日 20:00 更可在群内参与抽奖',
+        '- 永久解锁兑换码 ¥79.00，限量 95 个\n- 一年订阅兑换码 ¥32.00\n- 11 月 6 日 20:00 更可在群内参与抽奖，以更低价格购买',
       urlString: 'https://okr.vision/zh/blog/2021/11/03/promo-campaign',
       buttonText: '活动详情',
       proEntranceSecondaryText: '🔥 促销中',
